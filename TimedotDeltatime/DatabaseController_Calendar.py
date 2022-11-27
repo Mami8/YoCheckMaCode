1e227c75c19e3e8630c00b3d8b5938048d3e85a9
Mamig <muhammedfurkanekici123@gmail.com>
2022-11-27 09:18:11 +0300

    yooo

--- TimedotDeltatime/DatabaseController_Calendar.py
+++ TimedotDeltatime/DatabaseController_Calendar.py
@@ -2,7 +2,7 @@ import sqlite3 as sqlite
 
 
 def connect():
-    con = sqlite.connect("")
+    con = sqlite.connect("D:/YoCheckMaCode/TimedotDeltatime/Calendar.db")
     cursor = con.cursor()
 
     return con, cursor
@@ -21,32 +21,58 @@ def close_connection(connection, commit_mode=True):
 
 
 def data_retrieve():
+    """
+    Returns names of events, dates of events and notification types of events with same order.
+    """
+    # Connecting, getting needed data and closing the connection
     con, cursor = connect()
     db = cursor.execute("SELECT * FROM events").fetchall()
     con.close()
 
+    event_amount = 0
     names = []
     dates = []
     not_types = []
 
+    # Spliting data retrieved and returning in an order
     for i in db:
+        event_amount += 1
         names.append(i[1])
         dates.append(i[2])
         not_types.append(i[3])
 
-    return names, dates, not_types
+    return names, dates, not_types, event_amount
 
 
 def data_append(event_name, date, not_type):
+    """
+    Connection and getting data appended. This function needs four variables
+    The first one can be anything
+    Second one should come from function date_simplifier
+
+    The last one can currently get 2 values even tough It's a string. These are
+    (day_before, hour_before)
+    Meanings are the same with names. Values are set, day_before means one day before at 18:00
+    and hour_before means 2 hours before
+    """
     con, cursor = connect()
     cursor.execute('''INSERT INTO events (event_name, date, notification_type) 
     VALUES ("{}", "{}", "{}")'''.format(event_name, date, not_type))
+
     close_connection(con, True)
 
 
-def date_simplifier(day, month, year):
-    day.strip()
-    month.strip()
-    year.strip()
+def date_simplifier(day, month, year, hour):
+    """
+    Inputs should be like (dd, mm, yy, hh) and not like (dd, Month, yyyy, hh:mm:ss).
+    Hour should be in 24-hour format.
+    Return value can be used in data_append.
+    Make sure to put in numeric date. If It's already in dd;mm;yy form, don't use this function and feed
+    it directly to data_append. Program uses dd;mm;yy|hh format everywhere.
+    """
+    day = day.strip()
+    month = month.strip()
+    year = year.strip()
+    hour = hour.strip()
 
-    return day + ";" + month + ";" + year
+    return day + ";" + month + ";" + year + "|" + hour
---
+++ TimedotDeltatime/information_manager.py
@@ -0,0 +1,62 @@
+import atexit
+import locale
+
+import DatabaseController_Calendar as Database
+from datetime import datetime
+
+locale.setlocale(locale.LC_ALL, "Turkish_Turkey.1254")
+today = datetime.now()
+
+# [0] is numeric day, [1] is numeric month, [2] is two digit year and [3] is hour in 24-hour format
+current_date = today.strftime("%d;%m;%y;%H").split(";")
+
+events, raw_dates, raw_not_types, event_amount = Database.data_retrieve()
+dates = []
+times = []
+
+
+for i in raw_dates:
+    x, y = i.split("|")
+    dates.append(x)
+    times.append(y)
+
+
+@atexit.register
+def exit_handler():
+    pass
+
+
+def process():
+    print("\nFor the list of events, type events\n"
+          "To add event, type addevent\n")
+    secim = input(">> ").strip().lower()
+
+    match secim:
+        case "Ğ":
+            exit()
+        case "events":
+            i = 0
+            while i < event_amount:
+                print("-----------------------------------")
+                print("name = " + events[i], "\nDate = " + dates[i], "\nTime = " + times[i], "\nnot_type = " + raw_not_types[i])
+                i += 1
+        case "addevent":
+            name = input("Please enter the name of the event\n>> ").strip()
+            date = input("Please enter the date of the event in the form of dd/mm/yy\n>> ").strip().split("/")
+            time = input("Please enter the time of your event in 24-hour format hh\n>> ").strip()
+            not_type = input("Please enter the not_type of the event.( day_before or hour_before)").strip().lower()
+
+            if not_type != "day_before" and not_type != "hour_before":
+                print("Invlid not_type")
+            else:
+                simp_time = Database.date_simplifier(date[0], date[1], date[2], time)
+
+                correct = input(name, not_type, date, time, "\nIs the information correct?( Y/N )").lower()
+                if correct != "y" and correct != "n":
+                    return 1
+
+                elif correct == "y":
+                    Database.data_append(name, simp_time, not_type)
+
+                else:
+                    return 0
